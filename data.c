06fccbaa9c0033d2263eb4a8c663e5c61cc99364
mati2225 <28987896+mati2225@users.noreply.github.com>
2026-01-13 15:37:08 +0100

    Add main data structures and first functions

--- data.c
+++ data.c
@@ -1,2 +1,22 @@
 #include "data.h"
 
+const char* mech_class_to_str(enum MechClass s) {
+    switch (s) {
+        case SZTURMOWY:             return "SZTURMOWY";
+        case WSPARCIA_OGNIOWEGO:    return "WSPARCIA OGNIOWEGO";
+        case REKONESANSOWY:         return "REKONESANSOWY";
+        case OBRONNY:               return "OBRONNY";
+        default:                    return "BRAK";
+    }
+}
+
+const char* mech_technical_condition_to_str(enum MechTechnicalCondition s) {
+    switch (s) {
+        case SPRAWNY:           return "SPRAWNY";
+        case WYMAGA_PRZEGLADU:  return "WYMAGA PRZEGLADU";
+        case USZKODZONY:        return "USZKODZONY";
+        case W_NAPRAWIE:        return "W NAPRAWIE";
+        case W_DEMONTAZU:       return "W DEMONTAZU";
+        default:                return "NIEZNANY";
+    }
+}
--- data.h
+++ data.h
@@ -1,6 +1,32 @@
 #ifndef DATA_H
 #define DATA_H
 
+enum MechClass {
+    SZTURMOWY,
+    WSPARCIA_OGNIOWEGO,
+    REKONESANSOWY,
+    OBRONNY
+};
 
+enum MechTechnicalCondition {
+    SPRAWNY,
+    WYMAGA_PRZEGLADU,
+    USZKODZONY,
+    W_NAPRAWIE,
+    W_DEMONTAZU
+};
+
+typedef struct Mech {
+    char model[101];
+    enum MechClass class;
+    int reactor_power;
+    int armor_health;
+    int ammo;
+    char assigned_pilot[51];
+    enum MechTechnicalCondition condition;   
+} Mech;
+
+const char* mech_class_to_str(enum MechClass s);
+const char* mech_technical_condition_to_str(enum MechTechnicalCondition s);
 
 #endif
