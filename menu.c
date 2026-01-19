447f3440501de260e9899a691c743a04d69ab038
KubaKSalony334 <96623@student.pb.edu.pl>
2026-01-19 17:12:43 +0100

    Test different way of checking user input

--- menu.c
+++ menu.c
@@ -7,26 +7,26 @@ int menu_edit_mech_data();
 void menu_print_mech_data();
 void menu_display_mech_list();
 
-int menu_show() {           	
+int menu_show() {
 	printf("\n╔═[GLOWNE MENU]═══════════════════════════════╗\n║ 1. Dodaj nowego mecha                       ║\n║ 2. Edytuj wybrana jednostke                 ║\n║ 3. Wyswietl informacje o wybranej jednostce ║\n║ 4. Wyswietl wszystkie jednostki             ║\n║ 0. Wyjscie z programu                       ║\n╚═════════════════════════════════════════════╝\n>> ");
-	int choice;
-	scanf("%d", &choice);
+	char choice;
+	scanf("%c", &choice);
 	switch (choice) {
 		default:
 			printf("[!] Wybrana opcja nie istnieje\n");
 			return -1;
-		case 0:
+		case '0':
 			return 0;
-		case 1:
+		case '1':
 			menu_add_new_mech();
 			return 1;
-		case 2:
+		case '2':
 			while (menu_edit_mech_data() != 0) {}
 			return 2;
-		case 3:
+		case '3':
 			menu_print_mech_data();
 			return 3;
-		case 4:
+		case '4':
 			menu_display_mech_list();
 			return 4;
 	}
@@ -37,7 +37,7 @@ void menu_add_new_mech() {
 	char* class_converted = NULL;
 	int class;
 	while (true) {
-		printf("\n╔═[DODAWANIE MECHA]══════════════════╗\n║ WPROWADZ WYMAGANE DANE             ║\n╚════════════════════════════════════╝\n-= Model (unikalna nazwa mecha, identyfikator jednostki)  \n>> "); 
+		printf("\n╔═[DODAWANIE MECHA]══════════════════╗\n║ WPROWADZ WYMAGANE DANE             ║\n╚════════════════════════════════════╝\n-= Model (unikalna nazwa mecha, identyfikator jednostki)  \n>> ");
 		while (scanf(" %100[^\n]s", model) != 1) {
 			printf("[!] Wprowadzono bledne dane\n-= Model (unikalna nazwa mecha, identyfikator jednostki)  \n>> ");
 		}
@@ -52,11 +52,11 @@ void menu_add_new_mech() {
 		while (scanf(" %50[^\n]s", pilot) != 1) {
 			printf("[!] Wprowadzono bledne dane\n-= Przydzielony pilot (Imie i nazwisko)\n>> ");
 		}
-	
+
 		printf("\n╔═[DODAWANIE MECHA]══════════════════╗\n║ WERYFIKACJA WPROWADZONYCH DANYCH   ║\n╚════════════════════════════════════╝\n- MODEL: %s \n- KLASA: %s\n- PRZYDZIELONY PILOT: %s\nCzy wprowadzone dane sa poprawne? (1 - TAK; 0 - NIE): ", model, class_converted, pilot);
-		int option;
-		scanf(" %d", &option);
-		if (option != 0) { 
+		char option;
+		scanf(" %c", &option);
+		if (option != '0') {
 			break;
 		}
 		else {
@@ -67,36 +67,36 @@ void menu_add_new_mech() {
 
 int menu_edit_mech_data() {
 	printf("╔═[EDYTOWANIE JEDNOSTKI]═════════╗\n║ 1. Zmien klase mecha           ║\n║ 2. Zwieksz moc reaktora        ║\n║ 3. Zmniejsz moc reaktora       ║\n║ 4. Napraw pancerz              ║\n║ 5. Uzupelnij amunicje          ║\n║ 6. Przydziel pilota            ║\n║ 7. Zmien stan techniczny       ║\n║ 8. Usun jednostke z ewidencji  ║\n║ 0. Powrot do poprzedniego menu ║\n╚════════════════════════════════╝\n>> ");
-	int choice;
-	scanf("%d", &choice);
+	char choice;
+	scanf("%c", &choice);
 	switch (choice) {
 		default:
 			printf("[!] Wybrana opcja nie istnieje\n");
 			return -1;
-		case 0:
+		case '0':
 			return 0;
-		case 1: // Zmien klase mecha
+		case '1': // Zmien klase mecha
 			printf("- 1\n");
 			return 1;
-		case 2: // Zwieksz moc reaktora
+		case '2': // Zwieksz moc reaktora
 			printf("- 2\n");
 			return 2;
-		case 3: // Zmniejsz moc reaktora
+		case '3': // Zmniejsz moc reaktora
 			printf("- 3\n");
 			return 3;
-		case 4: // Napraw pancerz
+		case '4': // Napraw pancerz
 			printf("- 4\n");
 			return 4;
-		case 5: // Uzupelnij amunicje
+		case '5': // Uzupelnij amunicje
 			printf("- 5\n");
 			return 5;
-		case 6: // Przydziel pilota
+		case '6': // Przydziel pilota
 			printf("- 6\n");
 			return 5;
-		case 7: // Zmien stan techniczny
+		case '7': // Zmien stan techniczny
 			printf("- 7\n");
 			return 5;
-		case 8: // Usun jednostke z ewidencji
+		case '8': // Usun jednostke z ewidencji
 			printf("- 8\n");
 			return 5;
 	}
