ba3ec70d1f4a9b93b1b55eed651a7dfc4a8de3c8
mati2225 <28987896+mati2225@users.noreply.github.com>
2026-01-29 02:41:38 +0100

    Add better datagen

--- datagen.c
+++ datagen.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "data.h"
 
-#define ENTRIES 4
+#define ENTRIES 100
+
+void generate_mechs(mech_object* mechs, size_t count);
+
+void generate_mechs(mech_object* mechs, size_t count) {
+    const char* prefixes[] = {
+        "ATLAS", "TITAN", "RAVEN", "PHOENIX", "ODIN",
+        "ARES", "NEMESIS", "HADES", "ZEUS", "ORION"
+    };
+
+    const char* cores[] = {
+        "NOVA", "IRON", "VOID", "STEEL", "STORM",
+        "FURY", "ECHO", "EMBER", "SHADOW", "VORTEX"
+    };
+
+    const char* suffixes[] = {
+        "MK", "X", "PRIME", "CORE", "UNIT",
+        "FRAME", "GEAR", "DRIVE", "PROTO", "ALPHA"
+    };
+
+    const char* pilots[] = {
+        "Kowalski", "Nowak", "Raven", "Wolf", "Shadow",
+        "Orion", "Vega", "Hunter", "Blaze", "Steel"
+    };
+
+    size_t p_count = sizeof(prefixes) / sizeof(prefixes[0]);
+    size_t c_count = sizeof(cores) / sizeof(cores[0]);
+    size_t s_count = sizeof(suffixes) / sizeof(suffixes[0]);
+    size_t pilot_count = sizeof(pilots) / sizeof(pilots[0]);
+
+    srand((unsigned)time(NULL));
+
+    for (size_t i = 0; i < count; i++) {
+        snprintf(mechs[i].model, sizeof(mechs[i].model),
+                 "%s-%s-%s-%03zu",
+                 prefixes[rand() % p_count],
+                 cores[rand() % c_count],
+                 suffixes[rand() % s_count],
+                 i + 1);
+
+        mechs[i].type = rand() % 4;
+        mechs[i].reactor_power = rand() % 101;
+        mechs[i].armor_health = rand() % 1001;
+        mechs[i].ammo = rand() % 1001;
+
+        strncpy(mechs[i].assigned_pilot,
+                pilots[rand() % pilot_count],
+                sizeof(mechs[i].assigned_pilot) - 1);
+        mechs[i].assigned_pilot[sizeof(mechs[i].assigned_pilot) - 1] = '\0';
+
+        mechs[i].condition = rand() % 5;
+    }
+}
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -9,15 +64,11 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	const char* path = argv[1];
-    mech_object objects[ENTRIES] = {
-        {"test1", 1, 100, 100, 100, "imie nazwisko", 0},
-        {"test2", 0, 0, 26, 10, "", 3},
-        {"abcdefghijklmnopqrstuvwxyz", 2, 50, 33, 45, "imie nazwisko", 1},
-        {"test3", 0, 100, 1, 90, "pilot", 3}
-    };
-
+    const char* path = argv[1];
     FILE* fptr = fopen(path, "wb");
+    mech_object objects[ENTRIES];
+    generate_mechs(objects, ENTRIES);
+
     if (fptr == NULL) {
         perror("[!] Wystapil blad podczas otwierania pliku");
         return 1;
@@ -38,7 +89,7 @@ int main(int argc, char** argv) {
 
     size_t read = fread(backup, sizeof(mech_object), ENTRIES, fptr);
     if (read != ENTRIES) {
-        fprintf(stderr, "Blad: Odczytano tylko %zu elementow\n", written);
+        fprintf(stderr, "Blad: Odczytano tylko %zu elementow\n", read);
     }
     fclose(fptr);
 
